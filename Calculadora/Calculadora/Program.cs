c30ca6649905910b42305c1a54b94fd5a3b3007e
Gala <galamagdalena@hotmail.com>
2020-03-27 00:28:20 -0300

    Calculadora

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+            string signo;
+            int resultado;
+
+            Console.WriteLine("Ingrese un numero: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la operacion a realizar: ");
+            signo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese un numero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (signo == "+")
+            {
+                resultado = numero1 + numero2;
+                Console.WriteLine("El resultado es: " + resultado);
+            }
+
+            if (signo == "-")
+            {
+                resultado = numero1 - numero2;
+                Console.WriteLine("El resultado es: " + resultado);
+            }
+
+            if (signo == "*")
+            {
+                resultado = numero1 * numero2;
+                Console.WriteLine("El resultado es: " + resultado);
+            }
+
+            if (signo == "/" && numero2 == 0)
+            {
+                Console.WriteLine("No es posible dividir por cero");
+            }
+
+            if (signo == "/")
+            {
+                resultado = numero1 / numero2;
+                Console.WriteLine("El resultado es: " + resultado);
+            }
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
